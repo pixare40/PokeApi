d990b7da6e11febbe99537a638b5734dd62501fe
kabajie <Kabaji.Egara@derivco.co.uk>
2022-02-08 21:23:25 +0000

    test: Added pokemon controller tests

--- PokeApiTests/PokeApiController.Tests/PokemonControllerShould.cs
+++ PokeApiTests/PokeApiController.Tests/PokemonControllerShould.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Moq;
 using NUnit.Framework;
 using PokeApi.Controllers;
+using PokeApi.Models;
 using PokeApi.PokemonManagement;
 using PokeApi.ResponseModels;
 using System;
@@ -54,6 +55,39 @@ namespace PokeApiTests.PokeApiController.Tests
             var result = await controller.GetAsync("test");
 
             Assert.That(result.Result, Is.TypeOf<OkObjectResult>());
+        }
+
+        [Test]
+        public async Task ReturnValidPokemonModelIfEverythingIsOkAsync()
+        {
+            Mock<IPokemonService> pokemonService = new Mock<IPokemonService>();
+            Mock<IDescriptionProviderFactory> descriptionProviderFactory = new Mock<IDescriptionProviderFactory>();
+            Mock<IDescriptionService> descriptionService = new Mock<IDescriptionService>();
+
+            Habitat habitat = new Habitat { Name = "cave" };
+
+            pokemonService.Setup(x => x.GetPokemonAsync(It.IsAny<string>()))
+                .ReturnsAsync(() => new Pokemon { Name = "ditto", Habitat = habitat, IsLegendary = true });
+
+            descriptionService.Setup(x => x.GetDescriptionAsync(It.IsAny<Pokemon>(), It.IsAny<bool>()))
+                .ReturnsAsync(() => new DescriptionModel { Success = true, Description = "A test description" });
+
+            descriptionProviderFactory.Setup(x => x.GetDescriptor(It.IsAny<Pokemon>()))
+                .Returns(() => descriptionService.Object);
+
+            var services = new ServiceCollection();
+            services.AddMemoryCache();
+            var serviceProvider = services.BuildServiceProvider();
+
+            var memoryCache = serviceProvider.GetService<IMemoryCache>();
+
+            PokemonController controller = new PokemonController(pokemonService.Object,
+                descriptionProviderFactory.Object, memoryCache);
+
+            var result = await controller.GetAsync("ditto");
+
+            Assert.That(result.Result, Is.TypeOf<OkObjectResult>());
+
         }
     }
 }
